57e08430f22dd359b54102899e389bc84c6f5ed9
Ilya <60188664+seinja@users.noreply.github.com>
2021-09-30 01:20:08 +0300

    Work Start
    Add some basic func

---
+++ AstroBlast/Assets/Scripts/PlayerControoler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControoler : MonoBehaviour
+{
+    private Vector3 _mousePos;
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = _mousePos - transform.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+
+        _rb.rotation = angle;
+    }
+
+
+}
